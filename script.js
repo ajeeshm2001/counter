2643a6c7d2c5dcf1a2ec8b52893477e9a65bd35c
Ajeesh M <ajeeshm2001@gmail.com>
2022-12-03 11:46:42 +0530

    bootstrap

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-function plus(operator){
-    if(operator=='+'){
-        
-    let digit = document.getElementById('digit').innerHTML
-    document.getElementById('plus').style.background='green'
+function action(operator) {
+  if (operator == "+") {
+    let digit = document.getElementById("digit").innerHTML;
     digit++;
-    console.log(digit)
-    document.getElementById('digit').innerHTML=digit;
-    }else{
-        let digit = document.getElementById('digit').innerHTML
-        if(digit==0){
-            Swal.fire({
-icon: 'error',
-title: 'Oops...',
-text: 'You Cannot Enter Below 0',
-})
-        }else{
-            document.getElementById('minus').style.background='red'
-
-            digit--;
-    console.log(digit)
-    document.getElementById('digit').innerHTML=digit;
-        }
-
+    console.log(digit);
+    document.getElementById("digit").innerHTML = digit;
+  } else {
+    let digit = document.getElementById("digit").innerHTML;
+    if (digit == 0) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "You Cannot Enter Below 0",
+      });
+    } else {
+      digit--;
+      console.log(digit);
+      document.getElementById("digit").innerHTML = digit;
     }
-   
+  }
 }
 
-function reset(){
-    document.getElementById('digit').innerHTML=parseInt(0)
+function reset() {
+  document.getElementById("digit").innerHTML = parseInt(0);
 }
